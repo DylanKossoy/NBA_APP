085b5ab9697bacfc26118564e19634ab997efb78
Dylan Kossoy <dylankossoy@Dylans-MacBook-Air-3100.local>
2025-04-02 11:15:29 -0400

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Welcome from '../views/Welcome.vue'
+import Join from '../views/Join.vue'
+import SignIn from '../views/SignIn.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
+      name: 'welcome',
+      component: Welcome,
+    },
+    {
+      path: '/join',
+      name: 'join',
+      component: Join,
+    },
+    {
+      path: '/signin',
+      name: 'signin',
+      component: SignIn,
     },
   ],
 })
---
+++ src/views/Welcome.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div class="navbar">
+    <div class="logo-navbar">
+      <img src="../../public/basketballLogo.png" alt="" class="basketball-logo" />
+    </div>
+    <div class="links-navbar">
+      <RouterLink to="/join">Join</RouterLink>
+      <RouterLink to="/signin">Sign In</RouterLink>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.links-navbar {
+  display: flex;
+  margin-inline: 3rem;
+  gap: 2rem;
+}
+</style>
